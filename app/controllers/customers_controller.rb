7a0b970dad9348e73415b2e5c9d4a8d99301cb45
Pavel Zavalin <easter3000@gmail.com>
2019-09-02 15:14:09 +0400

    fix

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -16,8 +16,28 @@ class CustomersController < ApplicationController
   end
 
   # Показываем черный список Customer
-  def blacklisted
+  def blacklist
     render json: Customer.all.where(blacklisted: true)
+  end
+
+  # Добавляем Customer в черный список по телефону
+  def add_to_blacklist
+    customer = Customer.find_by_phone(params[:phone])
+    if customer.update(blacklisted: true)
+      render json: {message: "Клиент #{customer.name} добавлен в черный список."}
+    else
+      render json: {message: 'Не удалось добавить в черный список.'}
+    end
+  end
+
+  # Убираем Customer из черного списка
+  def remove_from_blacklist
+    customer = Customer.find(params[:id])
+    if customer.update(blacklisted: false)
+      render json: {message: "Клиент #{customer.name} исключен из черного списка."}
+    else
+      render json: {message: 'Не удалось исключить из черного списка.'}
+    end
   end
 
 
@@ -25,6 +45,6 @@ class CustomersController < ApplicationController
 
   def customer_params
     c_params = params.require(:customer).permit(:name, :phone, :description, :blacklisted)
-    c_params.select {|_, p| p.present?}
+    c_params.select { |_, p| p.present? }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :customers, only: [:index, :update] do
     collection do
-      get :blacklisted
+      get :blacklist
+      patch :add_to_blacklist
+      patch :remove_from_blacklist
     end
   end
 end
